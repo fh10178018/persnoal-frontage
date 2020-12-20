7281268f289e7e72db783e35e81d3b69eae8d618
Mertreci <fanghan@jianyi.tech>
2020-12-20 21:09:31 +0800

    :rocket:

---
+++ src/components/common/index.ts
@@ -0,0 +1,12 @@
+import MyButton from './MyButton'
+import MyInput from './MyInput'
+import MyTextArea from './MyTextArea'
+import MyIconButton from './MyIconButton'
+import MyRibbon from './MyRibbon'
+export {
+  MyButton,
+  MyInput,
+  MyTextArea,
+  MyIconButton,
+  MyRibbon
+}
