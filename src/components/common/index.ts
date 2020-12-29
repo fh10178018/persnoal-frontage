9d00dc4568e6440a4a3a97883d1f2849e98b4333
Mertreci <fanghan@jianyi.tech>
2020-12-29 12:19:49 +0800

    :new: add the dependent

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -3,10 +3,14 @@ import MyInput from './MyInput'
 import MyTextArea from './MyTextArea'
 import MyIconButton from './MyIconButton'
 import MyRibbon from './MyRibbon'
+import MyTag from './MyTag'
+import MyModal from './MyModal'
 export {
   MyButton,
   MyInput,
   MyTextArea,
   MyIconButton,
-  MyRibbon
+  MyRibbon,
+  MyTag,
+  MyModal
 }
