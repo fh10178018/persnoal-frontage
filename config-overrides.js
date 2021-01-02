6f88438d7166ec93cac4dbff70d362dcc1a2f704
fh10178018 <1206758827@qq.com>
2021-01-03 00:43:34 +0800

    :wrench:  implement the compression plugin

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,15 @@
 const { override, fixBabelImports } = require("customize-cra");
+const rewireCompressionPlugin = require('react-app-rewire-compression-plugin');
+
+// 在这里修改cra默认的插件配置项
+const replaceConfig = () => (config,env) => {
+  config = rewireCompressionPlugin(config, env, {
+    test: /\.js(\?.*)?$/i,
+    cache: true
+  })
+  // 需要返回config对象
+  return config;
+};
 
 module.exports = override(
   fixBabelImports("antd", {
@@ -10,5 +21,7 @@ module.exports = override(
     libraryName: "react-use",
     libraryDirectory: "lib",
     camel2DashComponentName: false,
-  })
+  }),
+  replaceConfig()
 );
+
