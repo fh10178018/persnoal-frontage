daa9f57005cf429e58e4e25d0bd143d3738741a9
fh10178018 <1206758827@qq.com>
2021-01-03 20:02:09 +0800

    :bug: change height scale

--- src/utils/getBoxHeight.ts
+++ src/utils/getBoxHeight.ts
@@ -2,7 +2,7 @@ import useWinSize from "./useWinSize"
 
 export default function useBoxHeight() {
   const { width, height } = useWinSize()
-  const bottom = Math.floor((width / 1235) * 237)
+  const bottom = Math.floor((width / 1235) * 200)
   return height - bottom
 }
 
