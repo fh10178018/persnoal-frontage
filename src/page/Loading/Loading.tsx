5d2fa25f75a6924a107ad6af9494f88fd90009c9
fh10178018 <1206758827@qq.com>
2021-01-18 12:01:06 +0800

    :new: use custom animations

--- src/page/Loading/Loading.tsx
+++ src/page/Loading/Loading.tsx
@@ -1,8 +1,8 @@
-import { Spin } from 'antd'
 import React from 'react'
 import styled from 'styled-components'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../redux/reducers'
+import useWinSize from '../../utils/useWinSize'
 
 const LoadingWrap = styled.div.attrs(props => {
   return {
@@ -28,12 +28,29 @@ const LoadingWrap = styled.div.attrs(props => {
     return props.active ? 1 : 0
   }};
 `
+
+const Iframe = styled.iframe`
+  border:none;
+  background-color:gold;
+`
+
+const ProgressDiv = styled.div`
+  position: absolute;
+  font-size: x-large;
+  font-weight:bold;
+  bottom:10vh;
+  animation: wave 2s infinite ease-out;
+`
+
 const Loading: React.FC = () => {
   const curProgressNum = useSelector((state: RootState) => state.progressNum)
+  const size = useWinSize()
   const progress = useSelector((state: RootState) => state.loadingProgress)
   return (
-    <LoadingWrap >< Spin size="large" /><div>{progress[curProgressNum]}</div></LoadingWrap>
+    <LoadingWrap>
+      <Iframe title="loading" src={process.env.PUBLIC_URL + 'loading.svg'} width={size.width} height={size.height} />
+      <ProgressDiv>{progress[curProgressNum]}</ProgressDiv>
+    </LoadingWrap>
   )
-
 }
 export default Loading
